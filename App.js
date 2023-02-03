c938703c32d07522f157e7339a7c9b071eaa0a80
Tyrah <tyrah96@gmail.com>
2023-02-03 15:33:34 -0800

    Instagram clone initalization code with auth

--- App.js
+++ App.js
@@ -1,20 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+require("dotenv").config();
 
+import * as firebase from "firebase";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import LandingScreen from "./src/Components/Auth/Landing";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+
+  authDomain: process.env.AUTH_DOMAIN,
+
+  projectId: process.env.PROJECT_ID,
+
+  storageBucket: process.env.STORAGE_BUCKET,
+
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+
+  appId: process.env.APP_ID,
+
+  measurementId: process.env.MEASUREMENT_ID,
+};
+
+// not running any firebase instances at the moment
+if (firebase.apps.length === 0) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+const Stack = createStackNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Landing">
+        <Stack.Screen
+          name="Landing"
+          component={LandingScreen}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
